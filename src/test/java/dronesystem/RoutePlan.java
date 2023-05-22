70683914e1045b5f1aa942012a9b847d8f52ee81
ah1m20 <aran.hakki@gmail.com>
2023-05-22 23:04:04 +0100

    updated waypoints

--- src/test/java/dronesystem/RoutePlan.java
+++ src/test/java/dronesystem/RoutePlan.java
@@ -2,9 +2,15 @@ package dronesystem;
 import ast.terms.Base;
 @Base public class RoutePlan {
 	private final DroneConnection connection = new DroneConnection();
-	private final Waypoint[] route = new Waypoint[100];
+	private final Waypoint[] route = new Waypoint[]{
+			new Waypoint(10,0,0),
+			new Waypoint(10,10,0),
+			new Waypoint(10,10,10),
+			new Waypoint(0,0,0)
+	};
+
 	private volatile int next = 0;
-	private volatile int home = 99;
+	private final int home = route.length-1;
 
 	public boolean hasNextWaypoint(){return next < route.length-1;}
 	public boolean noMoreWaypoints(){return next == route.length-1;}
