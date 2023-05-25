89e377f3bebf1682415e0da39cef4dbc6e73cd00
ah1m20 <aran.hakki@gmail.com>
2023-05-25 23:44:27 +0100

    added important comment for this branch.

--- src/test/java/dronesystem/Controller.java
+++ src/test/java/dronesystem/Controller.java
@@ -1,5 +1,12 @@
 package dronesystem;
 
+/*
+ * Work towards a new model for drone, which was developed by thinking in terms of Hierachical State Machines (HSM). This seem to be a close fit to Petra whilst being wildy used and being a simple visual tool to prototype the model.
+ * I believe the model is close to being verified and thinking in terms of HSMs helped this. Work should be done on seeing how to have an efficient thinking and modelling process to develop verified and validated Petra programs.
+ * The problem is how to go from the something which is poorly/unspecified within the domain, to a fully specified correct system in Petra.
+ * As Petra is new language / paradigm for formal verification it is difficult to map directly from ideas to verified models, without strong process for doing so.
+ */
+
 public class Controller {
 
 	private final Position position = new Position();
