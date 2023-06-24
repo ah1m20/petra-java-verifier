f0e95ac57ad2c2b4447baeb5b6d7c42cc570101d
ah1m20 <aran.hakki@gmail.com>
2023-06-24 21:04:07 +0100

    added comment as a reminder for how base objects should be checked

--- src/test/java/droneroutesystem/Temperature.java
+++ src/test/java/droneroutesystem/Temperature.java
@@ -4,6 +4,13 @@ import ast.terms.Initial;
 
 @Base public class Temperature {
 	private final DroneConnection connection = DroneConnection.getDroneConnection();
+
+	/*
+	 * Only the predicates in an external objects mentioned in the expressions are assumed to be disjoint, thus providing implicit views.
+	 *
+	 * This information is used for the predicates disjointedness and covering checks in the base objects.
+	 * Methods in base objects are computed from the pre/post-conditions and are not checked using compositions.
+	 */
 	@Initial
 	public boolean low() { return connection.low(); }
 	public boolean normal() { return connection.normal(); }
