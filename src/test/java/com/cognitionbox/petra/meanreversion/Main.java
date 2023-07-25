2ec6a773411828bc1c4358a1b6f205bae0a3b53d
ah1m20 <aran.hakki@gmail.com>
2023-07-25 23:18:05 +0100

    removed unverified systems from test folder as these have been placed in a dedicated dev branch for progressing with at a later date

--- src/test/java/com/cognitionbox/petra/meanreversion/Main.java
+++
@@ -1,9 +0,0 @@
-package com.cognitionbox.petra.meanreversion;
-
-import static com.cognitionbox.petra.ast.interp.util.Program.startReactive;
-
-public class Main {
-    public static void main(String[] args) {
-        startReactive(0,new MeanRev(), MarketData.getInstance());
-    }
-}
--- src/test/java/com/cognitionbox/petra/meanreversion/Trading.java
+++
@@ -1,9 +0,0 @@
-package com.cognitionbox.petra.meanreversion;
-
-import com.cognitionbox.petra.ast.terms.Base;
-
-@Base public class Trading {
-    private final MarketData marketData = MarketData.getInstance();
-    public boolean midAboveSMA(){return true;}
-    public boolean midBelowSMA(){return true;}
-}
